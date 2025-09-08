786166077ee17b9b5f4c71ec78109fc3ab1ba23e
bspippi1337 <bspippi1337@gmail.com>
2025-09-08 17:01:12 +0200

    Update guff_win.bat

--- guff_win.bat
+++ guff_win.bat
@@ -1,6 +1,11 @@
 @echo on
-powercfg.exe setactive scheme_current 
-powercfg.exe setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 100
+rem Hent aktivt strømskjema
+for /f "tokens=3" %%a in ('powercfg /getactivescheme') do set SCHEME=%%a
+
+rem Angi energisparer terskel til 100%
+powercfg.exe setdcvalueindex %SCHEME% SUB_ENERGYSAVER ESBATTTHRESHOLD 100
+
+rem Sett alle tidsavbrudd til 'aldri'
 powercfg.exe -x -monitor-timeout-ac 0
 powercfg.exe -x -monitor-timeout-dc 0
 powercfg.exe -x -disk-timeout-ac 0
@@ -9,3 +14,4 @@ powercfg.exe -x -standby-timeout-ac 0
 powercfg.exe -x -standby-timeout-dc 0
 powercfg.exe -x -hibernate-timeout-ac 0
 powercfg.exe -x -hibernate-timeout-dc 0
+pause
